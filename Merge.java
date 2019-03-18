73c4bf80e27b2a430771a65c5201ddeec7388bef
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-18 09:40:49 -0400

    Update Merge.java

--- Merge.java
+++ Merge.java
@@ -1,3 +1,12 @@
 public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
+    
+  }
+  private static void mergesortH(int[] data, int lo, int hi){
+   if(lo > hi)
+     return;
+    boolean isEven = (hi - lo + 1) % 2 == 0;
+  }
 
 }
