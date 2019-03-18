3eded194ea432edbc0508e0898f6e94c33d16a50
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-18 09:55:59 -0400

    Update Merge.java

--- Merge.java
+++ Merge.java
@@ -1,6 +1,9 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
+    if (data.length <= 1)
+      return;
+    mergesortH(data, 0, data.length - 1);
     
   }
   private static void mergesortH(int[] data, int lo, int hi){
