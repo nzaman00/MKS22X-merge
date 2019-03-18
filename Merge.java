c4395a3fedf4e7249aea26ff918fbdf12965b759
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-18 10:01:23 -0400

    initialized the sizes of new split arys

--- Merge.java
+++ Merge.java
@@ -10,6 +10,15 @@ public class Merge{
    if(lo > hi)
      return;
     boolean isEven = (hi - lo + 1) % 2 == 0;
+    int midIdx = 0;
+    int leftArySize = 0;
+    int rightArySize = 0;
+    if (isEven) {
+      midIdx = (hi - lo) / 2 + 1;
+      leftArySize = (hi - lo) / 2 + 1;
+      rightArySize = (hi - lo) / 2 + 1;
+    }
+    
   }
 
 }
